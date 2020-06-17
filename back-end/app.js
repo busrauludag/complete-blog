c36f641a27c3cbd0c64f2bfe06984a1838065afa
Busra Uludag <busrauludag13@gmail.com>
2020-06-17 22:22:23 +0300

    backend started, connetced mongodb

---
+++ back-end/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const { MONGOUIR } = require('./keys');
+const app = express();
+const PORT = 500;
+
+// Connection to MongoDB
+mongoose.connect(MONGOUIR, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('connected to MongoDB');
+})
+mongoose.connection.on('eroor', (err) => {
+  console.log(`Error: ${err}`);
+})
+
+// Routes
+app.get('/home', (req, res) => {
+  res.send('Home!');
+})
+
+app.get('/category', (req, res) => {
+  res.send('Category!');
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is started at ${PORT}`);
+})
---
+++ back-end/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  MONGOUIR : 'mongodb+srv://busrauludag:completeblog@cluster0-ukhzk.mongodb.net/complete-blog?retryWrites=true&w=majority',
+}
