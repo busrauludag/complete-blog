12c0e204bcbc26534033ab16697d248ea88c52d3
Busra Uludag <busrauludag13@gmail.com>
2020-06-20 12:30:13 +0300

    post created with category

--- back-end/routes/post.js
+++ back-end/routes/post.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const Post = mongoose.model('Post');
+const Category = mongoose.model('Category');
 
 router.get('/posts', (req, res) => {
-  Post.find()
+  Post
+    .find()
     .populate('category', '_id name')
     .then(posts => {
       res.json({ posts });
@@ -15,26 +17,36 @@ router.get('/posts', (req, res) => {
 })
 
 router.post('/new-post', (req, res) => {
-  const { title, description, imgUrl } = req.body;
+  const { title, description, imgUrl, category } = req.body;
 
-  if(!title || ! description || !imgUrl) {
+  if(!title || ! description || !imgUrl || !category) {
     res.json({ message: 'All fields are required!' });
   }
 
-  const post = new Post({
-    title,
-    description,
-    imgUrl
-  })
+  Category
+    .findOne({ _id : category.id })
+    .then(cat => {
+      
+      const post = new Post({
+        title,
+        description,
+        imgUrl,
+        category: cat
+      })
+    
+      post
+        .save()
+        .then(() => {
+          res.json({ msg: 'Post created' })
+        })
+        .catch((err) => {
+          console.log(err);
+        });
 
-  post
-    .save()
-    .then(() => {
-      res.json({ msg: 'Post created' })
     })
-    .catch((err) => {
+    .catch(err => {
       console.log(err);
-    });
+    })
 })
 
 module.exports = router;
